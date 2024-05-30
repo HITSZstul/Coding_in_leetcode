ed799120d1252961ae617e0038d92fa6d93c5b33
Simon <1907853454@qq.com>
2024-05-30 20:02:21 +0800

    try

--- 2_num_sum.cpp
+++ 2_num_sum.cpp
@@ -1,5 +1,6 @@
 #include <iostream>  
 #include <unordered_map>  
+#include <vector>
   
 int main() {  
     std::unordered_map<std::string, int> hashtable;  
@@ -23,3 +24,17 @@ int main() {
   
     return 0;  
 }
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>hashtable;
+        for(int i=0;i<nums.size();i++){
+            auto it = hashtable.find(target-nums[i]);
+            if(it!=hashtable.end()){
+                return {it->second,i};
+            }
+            hashtable[nums[i]]=i;
+        }
+        return {};
+    }
+};
